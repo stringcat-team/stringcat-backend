5e147fd5cedf68c7660fa63e536788a81f42563d
heejeong <jennachoi27@gmail.com>
2022-01-03 15:46:57 +0900

    package 구성

---
+++ stringcat-api/src/main/java/com/sp/api/auth/AuthReqDto.java
@@ -0,0 +1,14 @@
+package com.sp.api.auth;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotEmpty;
+
+public class AuthReqDto {
+
+}
---
+++ stringcat-api/src/main/java/com/sp/api/auth/AuthResDto.java
@@ -0,0 +1,4 @@
+package com.sp.api.auth;
+
+public class AuthResDto {
+}
