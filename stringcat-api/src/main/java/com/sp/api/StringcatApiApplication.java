eb152cc6cfc63ed6a533bf8dc2f5f523ee0c7854
YoonWonJun <ywj036@naver.com>
2022-01-03 21:51:19 +0900

    jasypt + datasource 설정

--- stringcat-api/src/main/java/com/sp/api/StringcatApiApplication.java
+++ stringcat-api/src/main/java/com/sp/api/StringcatApiApplication.java
@@ -3,7 +3,7 @@ package com.sp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sp")
 public class StringcatApiApplication {
 
     public static void main(String[] args) {
