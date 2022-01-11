226b5b71936fd7b878c2b6c96e3ec5b2af3f08ea
heejeong <jennachoi27@gmail.com>
2022-01-11 18:11:17 +0900

    DB 연동 에러

--- stringcat-api/src/test/java/com/sp/api/StringcatApiApplicationTests.java
+++ stringcat-api/src/test/java/com/sp/api/StringcatApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sp.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StringcatApiApplicationTests {
-/*
-    @Test
-    void contextLoads() {
-    }*/
 
 }
