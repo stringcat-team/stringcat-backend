9cee344c0301458c9d1ee77d84d0ef4e2c0d883f
YoonWonJun <ywj036@naver.com>
2022-01-14 20:00:26 +0900

    swagger, security 오류 해결

--- stringcat-api/src/main/java/com/sp/api/auth/security/config/SecurityConfig.java
+++ stringcat-api/src/main/java/com/sp/api/auth/security/config/SecurityConfig.java
@@ -1,22 +1,16 @@
-package com.sp.api.config;
+package com.sp.api.auth.security.config;
 
-import com.sp.api.auth.JwtAuthenticationFilter;
+import com.sp.api.auth.security.jwt.JwtAuthenticationFilter;
 import com.sp.api.common.utils.SpringProfile;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,12 +23,7 @@ import org.springframework.web.filter.CorsFilter;
         jsr250Enabled = true,
         prePostEnabled = true
 )
-@Profile({SpringProfile.DEV, SpringProfile.LOCALDEV, SpringProfile.LOCAL})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired PasswordEncoder passwordEncoder;
-
-    @Autowired UserDetailsService userDetailsService;
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
@@ -43,17 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public static final String[] IGNORE_PAGES = new String[]{
             "/h2-console/**",
-            "/v3/api-docs/**",
+            "/v2/api-docs",
             "/configuration/**",
             "/webjars/**",
             "swagger*/**"
     };
-
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 
     @Override
     public void configure(WebSecurity webSecurity) {
@@ -61,20 +44,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Add our custom JWT security filter
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/guest/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
