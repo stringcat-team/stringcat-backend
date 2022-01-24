258cd289cb8d76ff49e76ad421866c2c55c60428
heejeong <jennachoi27@gmail.com>
2022-01-24 16:31:47 +0900

    contextLoads()

--- stringcat-api/src/test/java/com/sp/api/StringcatApiApplicationTests.java
+++ stringcat-api/src/test/java/com/sp/api/StringcatApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StringcatApiApplicationTests {
 
-    @Test
+/*    @Test
     void contextLoads() {
-    }
+    }*/
 
 }
