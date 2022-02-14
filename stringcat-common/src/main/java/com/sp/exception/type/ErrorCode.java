3896bd01fad2994a1f9b262956826fcfd43354dd
YoonWonJun <ywj036@naver.com>
2022-02-14 20:29:14 +0900

    답변 등록/수정/삭제 기능 구현

--- stringcat-common/src/main/java/com/sp/exception/type/ErrorCode.java
+++ stringcat-common/src/main/java/com/sp/exception/type/ErrorCode.java
@@ -13,12 +13,15 @@ public enum ErrorCode {
 
     // 400 Bad Request
     VALIDATION_EXCEPTION(BAD_REQUEST, "VA01", "잘못된 요청입니다."),
+    EXISTS_ANSWER(BAD_REQUEST, "VA02", "이미 답변을 등록했습니다."),
+    SELF_WRITE(BAD_REQUEST, "VA03", "자신이 작성한 질문에 답변할 수 없습니다."),
 
     // 401 UnAuthorized
     UNAUTHORIZED_EXCEPTION(UNAUTHORIZED, "UA01", "다시 인증해주세요."),
 
     // 403 Forbidden
     FORBIDDEN_EXCEPTION(FORBIDDEN, "FB01", "허용되지 않는 요청입니다."),
+    MISMATCH_WRITER(FORBIDDEN, "FB02", "작성자가 아닙니다."),
 
     // 404 Not Found
     NOT_FOUND(HttpStatusCode.NOT_FOUND, "NF01", "존재하지 않습니다."),
