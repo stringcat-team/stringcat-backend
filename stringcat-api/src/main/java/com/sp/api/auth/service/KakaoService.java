20a7ec231d323e2b42538185c06a417914154678
heejeong-choi <jennachoi27@gmail.com>
2022-02-24 16:04:46 +0900

    연산자 -> append 메서드 변경

--- stringcat-api/src/main/java/com/sp/api/auth/service/KakaoService.java
+++ stringcat-api/src/main/java/com/sp/api/auth/service/KakaoService.java
@@ -3,10 +3,6 @@ package com.sp.api.auth.service;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.sp.api.auth.dto.KakaoResDto;
-import com.sp.api.auth.security.jwt.JwtTokenProvider;
-import com.sp.domain.user.User;
-import com.sp.domain.user.UserQuerydslRepositoryImpl;
-import com.sp.domain.user.UserRepository;
 import com.sp.exception.type.StringcatCustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,17 +25,7 @@ import java.net.URL;
 @RequiredArgsConstructor
 public class KakaoService {
 
-    private final Oauth2Client kakaoClient;
-    private final UserQuerydslRepositoryImpl userQuerydslRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
     private final RestTemplate restTemplate;
-
-    public KakaoResDto getUserInfo(String accessToken) {
-        KakaoResDto kakaoUser = getUserInfoByToken(accessToken);
-
-        return kakaoUser;
-    }
 
     private KakaoResDto getUserInfoByToken(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
@@ -61,57 +47,7 @@ public class KakaoService {
         String email = body.getJSONObject("kakao_account").getString("email");
 
         return new KakaoResDto(id, email);
-
     }
-
-//    public String getAccessToken(String code) {
-//        String accessToken = "";
-//        String reqURL = "https://kauth.kakao.com/oauth/token";
-//
-//        try {
-//            URL url = new URL(reqURL);
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//
-//            connection.setRequestMethod("POST");
-//            connection.setDoOutput(true);
-//
-//            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-//            StringBuilder sb = new StringBuilder();
-//            sb.append("&client_id=cdcbce63806f6483ae54d2083e234da1"); // TODO REST_API_KEY 입력
-//            sb.append(""); // TODO 인가코드 받은 redirect_uri 입력
-////            &redirect_uri=http://localhost:3000/auth/callback
-//            sb.append("&code=" + code);
-//            bw.write(sb.toString());
-//            bw.flush();
-//
-//            int responseCode = connection.getResponseCode();
-//            System.out.println(responseCode + "입니다!!!!!!!!");
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            String line = "";
-//            StringBuilder result = new StringBuilder();
-//
-//            while((line = br.readLine()) != null) {
-//                result.append(line);
-//            }
-//
-//            log.info("응답 바디 :: {}", result);
-//
-//            JsonParser parser = new JsonParser();
-//            JsonElement element = parser.parse(result.toString());
-//
-//            accessToken = element.getAsJsonObject().get("access_token").getAsString();
-//
-//            log.info("access token :: {}", accessToken);
-//
-//            br.close();
-//            bw.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return accessToken;
-//    }
 
     public String createKakaoUser(String accessToken) throws StringcatCustomException {
         String reqUrl = "https://kapi.kakao.com/v2/user/me";
@@ -159,7 +95,6 @@ public class KakaoService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 }
